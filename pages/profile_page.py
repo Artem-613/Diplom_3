3c1994359b343f099156e5768ea0093185fa4154
Artem Shorokhov <artem.shorokhov@bk.ru>
2025-12-09 17:02:00 +0400

    fix: исправлено нарушение принципов Page Object Model

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -1,64 +1,78 @@
 import allure
 
+from data import Urls
 from locators import ProfilePageLocators
 from pages.base_page import BasePage
-from pages.constructor_page import ConstructorPage
 
 
 class ProfilePage(BasePage):
 
-    @allure.step('Открываем Личный кабинет по ссылке на Главной странице')
+    @allure.step('Открываем страницу профиля')
     def open_profile_page(self):
-        constructor_page = ConstructorPage(self.driver)
-        constructor_page.open_profile_page_by_link()
+        """Открывает страницу профиля напрямую по URL"""
+        self.open_page(Urls.PROFILE_PAGE_URL)
+        self.wait_for_load_element(ProfilePageLocators.ORDER_HISTORY_LINK)
 
-    @allure.step('кликаем ссылку "Личный кабинет"')
+    @allure.step('кликаем ссылку "История заказов"')
     def click_order_history_link(self):
-        # кликаем ссылку "Личный кабинет"
+        """Кликает на ссылку 'История заказов' в профиле"""
         self.click_element_by_locator(ProfilePageLocators.ORDER_HISTORY_LINK)
 
     @allure.step('Проверяем, что раздел История заказов становится активным')
     def order_history_is_active(self):
-        return self.wait_for_text_in_classname(ProfilePageLocators.ORDER_HISTORY_LINK,
-                                               ProfilePageLocators.ORDER_HISTORY_IS_ACTIVE)
+        """Проверяет, что раздел 'История заказов' активирован"""
+        return self.wait_for_text_in_classname(
+            ProfilePageLocators.ORDER_HISTORY_LINK,
+            ProfilePageLocators.ORDER_HISTORY_IS_ACTIVE
+        )
 
     @allure.step('кликаем кнопку "Выход"')
     def click_exit_button(self):
-        # кликаем ссылку "Личный кабинет"
+        """Кликает на кнопку 'Выход' в профиле"""
         self.click_element_by_locator(ProfilePageLocators.EXIT_BUTTON)
 
-    #
-    # вспомогательные функции для других тестов
-    @allure.step('Получаем список элементов страницы с номерами заказов')
+    # ========== Вспомогательные методы ==========
+    @allure.step('Получаем список элементов с номерами заказов')
     def __get_order_history_elements(self):
+        """Приватный метод: получает все элементы с номерами заказов"""
         return self.wait_for_load_all_elements(ProfilePageLocators.ORDER_HISTORY_ORDER_NUMBER)
 
-    @allure.step('Получаем элемент страницы с номером заказа')
+    @allure.step('Получаем первый элемент с номером заказа')
     def __get_order_history_first_element(self):
+        """Приватный метод: получает первый элемент с номером заказа"""
         return self.wait_for_load_element(ProfilePageLocators.ORDER_HISTORY_ORDER_NUMBER)
 
+    # ========== Публичные методы для тестов ==========
     @allure.step('Получаем список номеров заказов пользователя')
     def get_order_history_list(self):
-        # Открываем Личный кабинет по ссылке на Главной странице
-        self.open_profile_page()
-        # кликаем ссылку История заказов
+        """
+        Получает список номеров заказов из истории заказов
+        ВНИМАНИЕ: перед вызовом этого метода страница профиля должна быть уже открыта!
+        """
+        # Открываем раздел "История заказов"
         self.click_order_history_link()
-        # получаем элементы списка с номерами заказов
+        
+        # Получаем все элементы с номерами заказов
         elements = self.__get_order_history_elements()
-        #
+        
+        # Извлекаем текст из каждого элемента
         order_list = []
         for item in elements:
             order_list.append(item.text)
+        
         return order_list
 
     @allure.step('Получаем номер последнего заказа пользователя')
     def get_order_from_order_history(self):
-        # Открываем Личный кабинет по ссылке на Главной странице
-        self.open_profile_page()
-        # кликаем ссылку История заказов
+        """
+        Получает номер последнего заказа из истории заказов
+        ВНИМАНИЕ: перед вызовом этого метода страница профиля должна быть уже открыта!
+        """
+        # Открываем раздел "История заказов"
         self.click_order_history_link()
-        # получаем 1-й элемент списка с номерами заказов
+        
+        # Получаем первый (последний) элемент с номером заказа
         element = self.__get_order_history_first_element()
-        # получаем номер заказа
-        number = element.text
-        return number
+        
+        # Возвращаем номер заказа
+        return element.text
